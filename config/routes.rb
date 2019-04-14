6e5bbc7695dff8d97aae039951c04f59b6b752c6
Mohamed Ashraf <mohammed.ashraaf.ma@gmail.com>
2019-04-14 10:25:44 +0200

    Refactoring routes cause failed

--- config/routes.rb
+++ config/routes.rb
@@ -2,17 +2,24 @@ Rails.application.routes.draw do
   #This 
   root 'home#index'  
     scope '/api' do
-      post "students/signup", to: "students/registerations#create"
-      post "students/login", to: "students/sessions#create"
-      delete "students/logout", to: "students/sessions#destroy" 
-      post "admins/signup", to: "admins/registerations#create"
-      post "admins/login", to: "admins/sessions#create"
-      delete "admins/logout", to: "admins/sessions#destroy"   
-      post "teachers/signup", to: "teachers/registerations#create"
-      post "teachers/login", to: "teachers/sessions#create"
-      delete "teachers/logout", to: "teachers/sessions#destroy"
-      put "admins/students/:id", to: "admins/students#update"
-      put "admins/courses/:id", to: "courses#update"
+      # post "students/signup", to: "students/registerations#create"
+      # post "students/login", to: "students/sessions#create"
+      # delete "students/logout", to: "students/sessions#destroy"
+      # post "admins/signup", to: "admins/registerations#create"
+      # post "admins/login", to: "admins/sessions#create"
+      # delete "admins/logout", to: "admins/sessions#destroy"   
+      # post "teachers/signup", to: "teachers/registerations#create"
+      # post "teachers/login", to: "teachers/sessions#create"
+      # delete "teachers/logout", to: "teachers/sessions#destroy"
+      # put "admins/students/:id", to: "admins/students#update"
+      # put "admins/courses/:id", to: "courses#update"
+      devise_for :students,:path_names => { :sign_in => "login", :sign_out => "logout", :sign_up => "register" }, controllers: { registrations: 'students/registrations' , sessions: 'students/sessions'}
+      devise_for :admins,:path_names => { :sign_in => "login", :sign_out => "logout", :sign_up => "register" }, controllers: { registrations: 'admins/registrations' , sessions: 'admins/sessions'}
+      devise_for :teachers,:path_names => { :sign_in => "login", :sign_out => "logout", :sign_up => "register" }, controllers: { registrations: 'teachers/registrations' , sessions: 'teachers/sessions'}
+
+
+      # devise_for :students, :path => 'students', 
+
       resources :courses, controller: 'courses/resources', only: [:index, :show, :create]
       resources :grades , controller: 'grades/resources', only: [:index, :show, :create]
     end
